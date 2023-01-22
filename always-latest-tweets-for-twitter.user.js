aaddcae9b21299a14a8986ebd6c9a7b33ee169bc
Gea-Suan Lin <gslin@gslin.org>
2023-01-22 15:13:51 +0800

    Process /home only.

--- always-latest-tweets-for-twitter.user.js
+++ always-latest-tweets-for-twitter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Always Latest Tweets for Twitter
 // @namespace    https://github.com/gslin/always-latest-tweets-for-twitter
-// @version      0.20230121.0
+// @version      0.20230122.0
 // @description  Auto-switch to latest tweets for Twitter
 // @author       Gea-Suan Lin <darkkiller@gmail.com>
 // @match        https://mobile.twitter.com/*
@@ -15,6 +15,11 @@
   'use strict';
 
   let ob = new window.MutationObserver(ml => {
+    // Only /home is processed.
+    if (!document.location.pathname.match(/^\/home/)) {
+      return;
+    }
+
     // If it's already in "Following" timeline, uninstall the observer.
     if (document.querySelector('nav[role="navigation"] div[role="presentation"]:nth-child(2) a[aria-selected="true"]')) {
       console.debug('Already on following timeline.');
